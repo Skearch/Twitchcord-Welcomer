8fd6a5f66555640d761726a356438f2991dfb866
skearchdev <skearch.dev@gmail.com>
2024-01-07 17:44:36 +0800

    w

---
+++ Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitchcord_Welcomer
+{
+    public class Configuration
+    {
+        public string DiscordToken { get; set; }
+        public string TwitchToken { get; set; }
+        public string TwitchName { get; set; }
+        public string TwitchChannelName { get; set; }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,31 @@
 ﻿using DSharpPlus;
+using Newtonsoft.Json;
 using TwitchLib.Client;
 
 namespace Twitchcord_Welcomer
 {
     public class Program
     {
-        //SETTINGS, its not recommended to save sensitive variables in the source code :)
-        public static string discordToken = ""; // Discord bot Token.
-        public static string twitchToken = ""; //Twitch bot's ACCESS TOKEN.
-        public static string twitchName = ""; //Twitch bot's name.
-        public static string twitchChannelName = ""; //Twitch name to send welcome messages in.
-
-        //VARIABLES
+        //SETTINGS
+        public static string discordToken = "";
+        public static string twitchToken = "";
+        public static string twitchName = ""; 
+        public static string twitchChannelName = "";
         public static DiscordClient? discordClient;
         public static TwitchClient? twitchClient;
         public static string? twitchChannel;
 
         static async Task Main(string[] args)
         {
+            string json = File.ReadAllText("config.json");
+
+            Configuration config = JsonConvert.DeserializeObject<Configuration>(json);
+
+            discordToken = config.DiscordToken;
+            twitchToken = config.TwitchToken;
+            twitchName = config.TwitchName;
+            twitchChannelName = config.TwitchChannelName;
+
             DiscordBot bot = new();
             var discordBotTask = bot.RunAsync();
 
